870103ec34819848f3ec3fe9a5c14b3ab90dc9c2
gitesh <jhagiteshkumar@gmail.com>
2021-07-04 20:47:35 +0530

    git ignore.

--- Encapsulation/src/com/encapsulation/Car.java
+++ Encapsulation/src/com/encapsulation/Car.java
@@ -1,6 +1,9 @@
+package com.encapsulation;
 
 public class Car {
-
+	
+	String modelName;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sfsf");
